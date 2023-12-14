3b6b52cd967392debe7dac1b78f3e5cb215468d3
Brendon Rofe <brendon.richards@hyvemobile.co.za>
2023-12-14 13:50:23 +0200

    Practice basic destructuring

---
+++ BasicDestructuring.js
@@ -0,0 +1,11 @@
+const person1 = { name: 'Alice', age: 30, job: 'Developer' }
+const person2 = { name: 'Bob', age: 35, job: 'Designer' }
+const { name: name1, age: age1, job: job1 } = person1;
+const { name: name2, age: age2, job: job2 } = person2;
+
+console.log(name1);
+console.log(age1);
+console.log(job1);
+console.log(name2);
+console.log(age2);
+console.log(job2);
